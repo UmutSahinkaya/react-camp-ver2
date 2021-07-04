a13904251c91746018d0691cc0515eb95a67f525
UmutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2021-07-04 13:59:03 +0300

    04.07.2021 13:57

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ProductList from "../pages/ProductList";
+import Categories from "./Categories";
+import { Grid, GridColumn } from 'semantic-ui-react'
+import { Route } from 'react-router-dom';
+import ProductDetail from '../pages/ProductDetail';
+import CartDetail from '../pages/CartDetail';
+export default function Dashboard() {
+    return (
+        <div>
+            <Grid>
+                <Grid.Row>
+                    <GridColumn width={4}>
+                        <Categories />
+                    </GridColumn>
+                    <GridColumn width={12}>
+                        <Route exact path="/" component={ProductList}/>
+                        <Route exact path="/products" component={ProductList}/>
+                        <Route path="/products/:name" component={ProductDetail}/>
+                        <Route path="/cart" component={CartDetail}/>
+                    </GridColumn>
+                </Grid.Row>
+            </Grid>
+
+
+        </div>
+    )
+}
